087aab790ecdad398f5839c069d99fa2edb84fe7
Vicky Choudhary <95609264+thevickeychaudhary@users.noreply.github.com>
2023-04-06 23:26:43 +0530

    Add files via upload

---
+++ while loop if esle.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i=0;
+int n;
+printf("enter the natural number : ");
+scanf("%d",&n);
+while(i<10)
+{
+if(i>10)
+{
+printf("the value of i is %d\n: ",i);
+}
+i++;
+}
+return 0;
+}
---
+++ while loop n numbwers..c,,.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i=0;
+int n;
+printf("enter the natural number : ");
+scanf("%d",&n);
+while(i<n)
+{
+printf("the value of i is %d\n: ",i);
+i++;
+}
+return 0;
+}
